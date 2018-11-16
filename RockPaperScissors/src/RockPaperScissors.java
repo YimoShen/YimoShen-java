4098cfc4ef3abaa6f108bc3b850646acd7cfe52a
YimoShen.java <19shenyimo@bprep.org>
2018-11-16 09:21:28 -0800

    Rock Paper Scissors Project

--- RockPaperScissors/src/RockPaperScissors.java
+++ RockPaperScissors/src/RockPaperScissors.java
@@ -1,24 +1,48 @@
 import java.util.Scanner;
 
 public class RockPaperScissors {
-    public static void main(String[] args) {
-       Scanner userinput = new Scanner (System.in);
-       String user = "";
-       String console = "";
-       int userScore = 0;
-       int consoleScore = 0;
-       while(playGame()=="y"){
-           if (userScore > 1 || consoleScore >1){
-               System.out.println("The game is over!");
-               if (userScore > consoleScore){
-                   System.out.println("User wins!");
-               } else {
-                   System.out.println("Console wins!");
-               }
-               printScore();
-           } else {
+    public static String user = " ";
+    public static String console = " ";
+    public static int userScore = 0;
+    public static int consoleScore = 0;
 
-           }
-       }
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String username = console.next();
+        System.out.print("How many games do you want to play? Choose either 3 or 5.");
+        String choice = console.next();
     }
 }
+        /* while (checkWinner()==){
+            if (userScore>3||consoleScore>3){
+                System.out.println("Game Over!");
+                if (userScore>consoleScore){
+                    System.out.println("User Wins!");
+                }
+                else {
+                    System.out.println("Console Wins!");
+                }
+                printScore();
+            }
+            else {
+                user = " ";
+                con = " ";
+                userScore = 0;
+                checkWinner();
+                System.out.println("User, take your pick! R/S");
+            }
+        }
+    }
+    public static void checkWinner(){
+    if (user==console){
+        System.out.println("Tie! No points given.");
+
+    if ((user==r&&con==s))||(user==s&&con==p)||(user==p&&con==r)){
+        System.out.println("User wins! 1 point given to the user");
+        userScore += 1;
+     }
+     if ((user==s&&con==r))||(user==p&&con==s)||(user==r&&con==s)){
+        System.out.println("Computer wins! 1 point given to the computer");
+        consoleScore += 1;
+     } */
