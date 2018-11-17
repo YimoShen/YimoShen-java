4547cfc3d09ee1d350cefbe0f1cbd74f31561c23
YimoShen.java <19shenyimo@bprep.org>
2018-11-17 11:16:22 -0800

    Rock Paper Scissors Project

--- RockPaperScissors/src/RockPaperScissors.java
+++ RockPaperScissors/src/RockPaperScissors.java
@@ -1,48 +1,63 @@
+/*
+*********************
+Yimo Shen
+AP Computer Science A
+Mrs. Transue
+November 17, 2018
+Rock Paper Scissors
+*********************
+*/
+
 import java.util.Scanner;
+import java.util.Random;
 
 public class RockPaperScissors {
-    public static String user = " ";
-    public static String console = " ";
-    public static int userScore = 0;
-    public static int consoleScore = 0;
-
+    public static int userScore = 0; //initializes user score in this game.
+    public static int consoleScore = 0; //initializes computer score in this game.
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.print("What is your name?");
-        String username = console.next();
+        String username = console.next(); //Allows user to type hs/her name/
+
+        String playAgain = "y"; //If the user wants to play again, entering "y' will resume the game.
         System.out.print("How many games do you want to play? Choose either 3 or 5.");
-        String choice = console.next();
-    }
-}
-        /* while (checkWinner()==){
-            if (userScore>3||consoleScore>3){
-                System.out.println("Game Over!");
-                if (userScore>consoleScore){
-                    System.out.println("User Wins!");
-                }
-                else {
-                    System.out.println("Console Wins!");
-                }
-                printScore();
-            }
-            else {
-                user = " ";
-                con = " ";
-                userScore = 0;
-                checkWinner();
-                System.out.println("User, take your pick! R/S");
+        int choice = console.nextInt(); //Allows the user to play either 3 games or 5 games.
+        while (playAgain.equals("y")){ //This while loop generates the process of the whole game.
+            System.out.println("What is your pick? Rock, paper or scissors? Enter 0 for rock, 1 for paper and 2 for scissors.");
+            System.out.println("Rock(0) beats scissors(2), paper(1) beats rock(0) and scissors(2) beats paper(1).");
+            int yourPick = console.nextInt();
+            System.out.println("You picked " + yourPick);
+            Random computer = new Random(); //Computer generates its choice randomly (0,1 and 2).
+            int computerPick = computer.nextInt(3);
+            System.out.println("Computer picked " + computerPick);
+            checkWinner(yourPick, computerPick); //Method that determines the winner of each round.
+            if (userScore==choice || consoleScore==choice){ //The game is over either when the computer wins overall or the user does.
+                System.out.println("Game Over! Thanks for playing.");
+                    if (userScore>consoleScore){ //The user wins overall if he/she wins more rounds
+                        System.out.println("User Wins!");
+                        break; //Exit the game.
+                    }
+                    else {
+                        System.out.println("Console Wins!"); ////The computer wins overall if it wins more rounds.
+                        break; //Exit the game.
+                    }
+            } else {
+                System.out.println(username + ", do you want to play again? y/n");//Ask user if wanting to play again.
+                playAgain = console.next();
             }
         }
     }
-    public static void checkWinner(){
-    if (user==console){
-        System.out.println("Tie! No points given.");
-
-    if ((user==r&&con==s))||(user==s&&con==p)||(user==p&&con==r)){
-        System.out.println("User wins! 1 point given to the user");
-        userScore += 1;
-     }
-     if ((user==s&&con==r))||(user==p&&con==s)||(user==r&&con==s)){
-        System.out.println("Computer wins! 1 point given to the computer");
-        consoleScore += 1;
-     } */
+    public static void checkWinner (int yourPick, int computerPick){ //Method that determines the winner of each round.
+        if (yourPick==computerPick) { //Tie game scenario and no one gets points.
+            System.out.println("Tie! No points given.");
+        }
+        if ((yourPick==0 && computerPick==2) || (yourPick==2 && computerPick==1) || (yourPick==1 && computerPick==0)){
+            System.out.println("User wins! 1 point given to the user");
+            userScore += 1; //The scenario when user wins one round and gets one point.
+        }
+        if ((yourPick==2 && computerPick==0) || (yourPick==1&&computerPick==2) || (yourPick==0&&computerPick==1)){
+            System.out.println("Computer wins! 1 point given to the computer");
+            consoleScore += 1; //The scenario when computer wins one round and gets one point.
+        }
+    }
+}
