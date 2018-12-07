583f6af24564c0a65f1bbf74aa83bfb5723271f2
YimoShen.java <19shenyimo@bprep.org>
2018-12-07 09:18:48 -0800

    Roll Dice

--- RollDice/src/RollDice.java
+++ RollDice/src/RollDice.java
@@ -1,39 +1,63 @@
 import java.util.Scanner;
+import java.util.Random;
+
 public class RollDice {
     public static final int DICENUM = 1;
-    String dice [][] = new String[6][5];
-    String gameDice [][] = new String[DICENUM][5];
-
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
         System.out.println("Do you want to roll a dice?");
-        String yourPick = console.next();
+        Scanner rollTheDice = new Scanner(System.in);
+        String yourPick = rollTheDice.next();
+        yourPick = yourPick.toLowerCase();
+        String dice[][] = defineDice();
+        printAllDice(dice);
 
         while (yourPick.equals("Yes")) {
-            defineDice();
-            printDice();
             rollDice();
+            System.out.println(rollDice());
+            printDice();
             drawLine();
-            System.out.println("Do you want to play again? Enter Yes or No.");
-            yourPick = console.next();
+            System.out.println("Do you want to roll again? Enter Yes or No");
+            yourPick = rollTheDice.next();
         }
-
     }
 
-    public static void defineDice() {
-        String TopBottom [] = {"-------"};
-        System.out.println("|     |");
-        System.out.println("|  *  |");
-        System.out.println("|     |");
+    public static String[][] defineDice() {
+        System.out.println("define dice");
+        String defineDice[][] = new String[5][6];
+        String topBottom[] = {" ------- ", " ------- ", " ------- ", " ------- ", " ------- ", " ------- "};
+        String topMiddle[] = {"|       |","| *     |", "| *     |","|  * *  |", "|  * *  |", "|  * *  |"};
+        String middleMiddle[] = {"|   *   |", "|       |", "|   *   |", "|       |", "|   *   |", "|  * *  |"};
+        String bottomMiddle[] = {"|       |", "|     * |", "|     * |", "|  * *  |", "|  * *  |", "|  * *  |"};
+        defineDice[0] = topBottom;
+        defineDice[1] = topMiddle;
+        defineDice[2] = middleMiddle;
+        defineDice[3] = bottomMiddle;
+        defineDice[4] = topBottom;
+        return(defineDice);
     }
+
     public static void printDice() {
-        System.out.println("print dice");
+        System.out.println("printDice");
     }
-    public static void rollDice() {
-        System.out.println("roll dice");
+
+    public static void printAllDice(String[][] dicePrinter) {
+        for (int diceNum = 0; diceNum < 6; diceNum++) {
+            for (int row = 0; row < 5; row++) {
+                System.out.println(dicePrinter[row][diceNum]);
+            }
+            System.out.println();
+        }
+        System.out.println("printAllDice");
+    }
+
+    public static int rollDice() {
+        Random roll = new Random();
+        int outcome = roll.nextInt(6) + 1;
+        System.out.println("rollDice");
+        return(outcome);
     }
     public static void drawLine() {
-        System.out.println("draw line");
+        System.out.println("drawLine");
     }
 }
 
