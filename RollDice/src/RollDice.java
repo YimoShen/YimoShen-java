1a5c706fe03a98caeaae583ce966a854bc3f29c0
YimoShen.java <19shenyimo@bprep.org>
2018-12-08 11:00:51 -0800

    Roll Dice

--- RollDice/src/RollDice.java
+++ RollDice/src/RollDice.java
@@ -1,28 +1,36 @@
+/*
+*********************
+Yimo Shen
+Mrs.Transue
+AP Computer Science A
+December 8, 2018
+Roll Dice Project
+*********************
+*/
+
 import java.util.Scanner;
 import java.util.Random;
 
 public class RollDice {
-    public static final int DICENUM = 1;
+    public static final int DICENUM = 3;
     public static void main(String[] args) {
-        System.out.println("Do you want to roll a dice?");
+        System.out.println("Do you want to roll a dice?"); // interactive program that asks for user input
         Scanner rollTheDice = new Scanner(System.in);
         String yourPick = rollTheDice.next();
         yourPick = yourPick.toLowerCase();
-        String dice[][] = defineDice();
-        printAllDice(dice);
 
-        while (yourPick.equals("Yes")) {
-            rollDice();
-            System.out.println(rollDice());
-            printDice();
-            drawLine();
-            System.out.println("Do you want to roll again? Enter Yes or No");
-            yourPick = rollTheDice.next();
+
+        while (yourPick.equals("yes")) { // when user enters yes, the for loop below executes.
+            for (int i = 0; i < DICENUM; i++){ //it will print the amount of dices the class constant DICENUM INDICATES.
+                printDice();
+            }
+            drawLine(); //divider
+            System.out.println("Do you want to roll again? Enter yes or no"); 
+            yourPick = rollTheDice.next(); //interactive program that asks if user wants to roll again
         }
     }
 
-    public static String[][] defineDice() {
-        System.out.println("define dice");
+    public static String[][] defineDice() { //prints out all the dices from 1 to 6
         String defineDice[][] = new String[5][6];
         String topBottom[] = {" ------- ", " ------- ", " ------- ", " ------- ", " ------- ", " ------- "};
         String topMiddle[] = {"|       |","| *     |", "| *     |","|  * *  |", "|  * *  |", "|  * *  |"};
@@ -36,28 +44,22 @@ public class RollDice {
         return(defineDice);
     }
 
-    public static void printDice() {
-        System.out.println("printDice");
-    }
-
-    public static void printAllDice(String[][] dicePrinter) {
-        for (int diceNum = 0; diceNum < 6; diceNum++) {
-            for (int row = 0; row < 5; row++) {
-                System.out.println(dicePrinter[row][diceNum]);
-            }
-            System.out.println();
+    public static void printDice() { //prints out one specific dice randomly by using roll dice method.
+        String dice[][] = defineDice();
+        int roll = rollDice();
+        System.out.println(roll);
+        for (int i = 0; i < 5; i++){
+            System.out.println(dice[i][roll-1]);
         }
-        System.out.println("printAllDice");
     }
 
-    public static int rollDice() {
+    public static int rollDice() { //generates the number of the dice randomly
         Random roll = new Random();
         int outcome = roll.nextInt(6) + 1;
-        System.out.println("rollDice");
         return(outcome);
     }
-    public static void drawLine() {
-        System.out.println("drawLine");
+    public static void drawLine() { //provides the divider between each roll.
+        System.out.println("---------");
     }
 }
 
