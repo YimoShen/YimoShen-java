cb087d3a444d4eecfca393e5e0e9a96a1f238e6b
YimoShen.java <19shenyimo@bprep.org>
2018-12-13 09:24:11 -0800

    frac calc

--- fractionCalculator/src/fractionCalculator.java
+++ fractionCalculator/src/fractionCalculator.java
@@ -13,29 +13,68 @@ public class fractionCalculator {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Enter Start to begin program.");
-        String looper = console.next();
+        String input = "Start";
+        String token;
 
-        while(looper.equals("Start")){
-            System.out.println("Enter something");
-            looper = console.nextLine();
+        while(input.equals("Start")){
+            System.out.println("Enter 3 words");
+            token = console.next();
+            System.out.println(token);
+
+
+           /*
             int j = -1;
             int n = 0;
+            String [] fraction = new String [3];
             for(int i = 0; i < looper.length(); i++){
                 j++;
                 if (looper.charAt(i)==' ') {
-                    System.out.println(looper.substring(i - j, i));
+                    System.out.println(looper.substring(n,i));
                     j = 1;
                     n++;
                 }
                 if (n == 2){
                     System.out.println(looper.substring(i+1));
+                    fraction[n] = looper.substring(i);
+                    n++;
                 }
             }
+            if (looper.equals("Exit")) {
+                break;
+            }
+            */
+
         }
+    }
+    public static String add(final int firstNume, final int firstDenom,
+                             final int secondNume, final int secondDenom) {
+        return "";
+    }
 
+    public static String subtract(final int firstNume, final int firstDenom,
+                                  final int secondNume, final int secondDenom) {
+        return "";
+    }
 
+    public static String multiply(final int firstNume, final int firstDenom,
+                                  final int secondNume, final int secondDenom) {
+        return "";
+    }
 
+    public static String divide(final int firstNume, final int firstDenom,
+                                final int secondNume, final int secondDenom) {
+        return "";
+    }
 
+    public static int parseWhole(final String fraction) {
+        return 0;
+    }
 
+    public static int parseNume(final String fraction) {
+        return 0;
+    }
+
+    public static int parseDenom(final String fraction) {
+        return 0;
     }
 }
