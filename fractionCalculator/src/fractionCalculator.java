eb2086267b6ee637232ac72cd449d0f191df2f8c
YimoShen.java <19shenyimo@bprep.org>
2019-01-04 09:18:29 -0800

    frac calc

--- fractionCalculator/src/fractionCalculator.java
+++ fractionCalculator/src/fractionCalculator.java
@@ -11,18 +11,27 @@ Fraction Calculator Project
 import java.util.*;
 public class fractionCalculator {
     public static void main(String[] args) {
+
         Scanner console = new Scanner(System.in);
-        System.out.println("Enter Start to begin program.");
+        System.out.println("Enter 3 things or quit.");
         String input = "Start";
-        String token;
 
-        while(input.equals("Start")){
-            System.out.println("Enter 3 words");
-            token = console.next();
-            System.out.println(token);
+        while (!input.equals("Quit")) {
+            input = console.next();
+            System.out.println(input);
+            String token [] = new String [3];
+            token [0] = input;
+            token [1] = input;
+            token [2] = input;
+
+        }
 
 
-           /*
+    }
+}
+
+
+        /*
             int j = -1;
             int n = 0;
             String [] fraction = new String [3];
@@ -42,27 +51,22 @@ public class fractionCalculator {
             if (looper.equals("Exit")) {
                 break;
             }
-            */
 
-        }
+
     }
-    public static String add(final int firstNume, final int firstDenom,
-                             final int secondNume, final int secondDenom) {
+    public static String add(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
+        return "";
+
+
+    public static String subtract(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
         return "";
     }
 
-    public static String subtract(final int firstNume, final int firstDenom,
-                                  final int secondNume, final int secondDenom) {
+    public static String multiply(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
         return "";
     }
 
-    public static String multiply(final int firstNume, final int firstDenom,
-                                  final int secondNume, final int secondDenom) {
-        return "";
-    }
-
-    public static String divide(final int firstNume, final int firstDenom,
-                                final int secondNume, final int secondDenom) {
+    public static String divide(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
         return "";
     }
 
@@ -78,3 +82,4 @@ public class fractionCalculator {
         return 0;
     }
 }
+*/
